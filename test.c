46caf6ccbfdba137a690b72a4d6eac03b3c10372
varshithreddy37 <psaivarshithreddy@gmail.com>
2025-10-12 00:23:20 +0530

    Updated test.c file with actual split-wise program logic

--- test.c
+++ test.c
@@ -1,1 +1,20 @@
-// Dum,y test file
+#include<stdio.h>
+int main()
+{
+    float bills[5], total_bill = 0, each_share;
+    int people;
+    for(int i = 0; i < 5; i++)
+    {
+        printf("Enter total bill amount %d: $", i + 1);
+        scanf("%f", &bills[i]);
+        total_bill += bills[i]; // Add to total
+    }
+    printf("Enter total number of people;");
+    scanf("%d", &people);
+    
+    each_share = total_bill/people;
+    
+    printf("\ntotal bill: $%.2f", total_bill);
+    printf("\nEach person should pay: $%.2f\n", each_share);
+    return 0;
+}
